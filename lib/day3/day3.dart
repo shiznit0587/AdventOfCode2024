c22489b33acdeed3c90aa3d364149b3b39df43f6
Philip Stachowski <17034738+shiznit0587@users.noreply.github.com>
2024-12-02 15:08:16 -0500

    Stubbed Day 3

--- lib/day3/day3.dart
+++ lib/day3/day3.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 Future<void> run() async {
   final lines = utf8.decoder
-      .bind(File("lib/day2/input.txt").openRead())
+      .bind(File("lib/day3/input.txt").openRead())
       .transform(const LineSplitter());
 
   print('  Day 3 - Part 1');
