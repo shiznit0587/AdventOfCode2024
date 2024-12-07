385f5bcd922c30797bc320f903d02935ee565047
Philip Stachowski <17034738+shiznit0587@users.noreply.github.com>
2024-12-07 15:18:11 -0500

    Finished Day 6

--- bin/aoc.dart
+++ bin/aoc.dart
@@ -3,6 +3,7 @@ import 'package:aoc/day2/day2.dart' as day2;
 import 'package:aoc/day3/day3.dart' as day3;
 import 'package:aoc/day4/day4.dart' as day4;
 import 'package:aoc/day5/day5.dart' as day5;
+import 'package:aoc/day6/day6.dart' as day6;
 
 void main(List<String> arguments) async {
   print("\n🎅🎅🎅🎅🎅 ADVENT OF CODE 2024 🎅🎅🎅🎅🎅\n");
@@ -14,6 +15,7 @@ void main(List<String> arguments) async {
   await runDay(3, day3.run);
   await runDay(4, day4.run);
   await runDay(5, day5.run);
+  await runDay(6, day6.run);
 
   print('Total Time = ${stopwatch.elapsed}\n');
 }
---
+++ lib/day6/day6.dart
@@ -0,0 +1,86 @@
+import '../shared.dart';
+
+class Knight {
+  Coords pos;
+  Direction dir;
+
+  Knight({required this.pos, required this.dir});
+
+  factory Knight.from(Knight other) => Knight(pos: other.pos, dir: other.dir);
+
+  @override
+  operator ==(other) => other is Knight && other.pos == pos && other.dir == dir;
+
+  @override
+  int get hashCode => Object.hash(pos, dir);
+}
+
+Future<void> run() async {
+  final map = await readDay(6);
+
+  print('  Day 6 - Part 1');
+
+  Knight knight = Knight(pos: (x: 0, y: 0), dir: Direction.up);
+  Coords start = (x: 0, y: 0);
+
+  for (int x = 0; x < map.length; ++x) {
+    var y = map[x].indexOf('^');
+    if (y > -1) {
+      start = knight.pos = (x: x, y: y);
+      break;
+    }
+  }
+
+  Set<Coords> visited = {};
+  while (true) {
+    visited.add(knight.pos);
+
+    var next = knight.pos.go(knight.dir);
+    if (!map.inside(next)) {
+      break;
+    }
+
+    if (map.at(next) == '#') {
+      knight.dir = knight.dir.turnRight();
+    } else {
+      knight.pos = next;
+    }
+  }
+
+  print('  Visited = ${visited.length}');
+
+  print('  Day 6 - Part 2');
+
+  int loopsFound = 0;
+  for (int x = 0; x < map.rows; ++x) {
+    for (int y = 0; y < map.cols; ++y) {
+      if (map[x][y] == '.') {
+        Coords obstruction = (x: x, y: y);
+
+        knight = Knight(pos: start, dir: Direction.up);
+        Set<Knight> visitedB = {};
+
+        while (true) {
+          visitedB.add(Knight.from(knight));
+
+          var next = knight.pos.go(knight.dir);
+          if (!map.inside(next)) {
+            break;
+          }
+          if (visitedB.contains(Knight(pos: next, dir: knight.dir))) {
+            loopsFound++;
+            break;
+          }
+
+          if (map.at(next) == '#' || next == obstruction) {
+            knight.dir = knight.dir.turnRight();
+          } else {
+            knight.pos = next;
+          }
+        }
+      }
+    }
+  }
+
+  print('  Loops Found = $loopsFound');
+}
